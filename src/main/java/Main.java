4a42dc5aad52490f5d7ca4ca0d6ab753df88adc5
jdbs <krivettskiy@gmail.com>
2021-01-24 18:07:24 +0200

    Added to table via console.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,4 @@
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -23,28 +21,43 @@ public class Main {
     private void run() throws SQLException {
         try (Connection connection = DriverManager.getConnection("jdbc:sqlite:students.db")) {
             while (!"6".equals(number)) {
-                System.out.println("menu");
+                System.out.println(menu);
                 number = scanner.nextLine().trim();
                 int choice = Integer.parseInt(number);
 
                 switch (choice) {
                     case 1:
+                        createTable(connection);
+                        System.out.println("Table has been created");
+                        break;
+                    case 2:
+                        addNewStudent(connection);
+                        System.out.println("Student added");
+                        break;
+                    case 3:
 
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    default:
+                        System.out.println("Incorrect value");
                 }
             }
         }
     }
 
     private final String menu = "Enter a number from 1 to 6:" +
-            " 1 - Create table" +
-            " 2 - Add new student" +
-            " 3 - Show all students sorted by name" +
-            " 4 - Table search" +
-            " 5 - Delete table" +
+            " 1 - Create table\n" +
+            " 2 - Add new student\n" +
+            " 3 - Show all students sorted by name\n" +
+            " 4 - Table search\n" +
+            " 5 - Delete table\n" +
             " 6 - Exit";
 
     private void createTable(Connection connection) throws SQLException {
-        final String createStudent = "CREATE TABLE IF NOT EXISTS students (" +
+        final String createStudentTable = "CREATE TABLE IF NOT EXISTS students (" +
                 " id INTEGER PRIMARY KEY AUTOINCREMENT," +
                 " name VARCHAR(30)" +
                 " lastname VARCHAR(30)" +
@@ -54,5 +67,24 @@ public class Main {
         try (Statement statement = connection.createStatement()) {
             statement.executeUpdate(createStudent);
         }
+    }
+
+    private void addNewStudent(Connection connection) throws SQLException {
+        System.out.println("Enter student name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter student lastane: ");
+        String lastname = scanner.nextLine();
+        System.out.println("Enter your groug: ");
+        String group = scanner.nextLine();
+        System.out.println("Enter student faculty: ");
+        String faculty = scanner.nextLine();
+
+        final String addStudent =
+                "INSERT INTO student (name, lastname, group, faculty" +
+                " VALUE ('" + name + "', '" + lastname + "', '" + group + "', '" + faculty + "')";
+
+        Statement statement = connection.createStatement();
+            statement.executeUpdate(addStudent);
+
     }
 }
